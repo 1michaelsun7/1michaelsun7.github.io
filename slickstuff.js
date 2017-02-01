0b063195be72885af43ec8059990008ed2aaba63
1michaelsun7 <micsun@mit.edu>
2017-02-01 16:26:30 -0500

    lazy loading

--- slickstuff.js
+++ slickstuff.js
@@ -3,44 +3,57 @@ $(document).ready(function(){
 	var adtYoutubeIds = ["WuulitZGico", "-nwZqIfPUtk", "-3g7kUQXOu8", "x5sw7ggoDXk", "Kj13ZgX_UQ4", "7Ff93a-lwdU", "YbuoNpmTG4M", "jXntqigsL_8",  "BlmyCVCZL3o"];
 	var danceCoverIds = ["7q3Si1SAq_I", "UJxaw20hM7E", "YLKhlJdUxVs"];
 	var bboyIds = [];
-	var tttContent = "";
+	var adtContent = "";
+	var bboyContent = "";
+	var coverContent = "";
+
+	var adtBool = false;
+	var bboyBool = false;
+	var coverBool = false;
 
 	$('#bboy').on("click", function(){
-		for (var i = 0; i<9; i++){
-			if (bboyIds[i] !== undefined){
-				tttContent += '<iframe title="YouTube video player" class="youtube-player" type="text/html" src="https://www.youtube.com/embed/'+bboyIds[i]+'" frameborder="0" allowFullScreen></iframe>';
-			} else {
-				tttContent += '<div class="novideo"><p>Coming Soon!</p></div>';
+		if (!bboyBool){
+			for (var i = 0; i<9; i++){
+				if (bboyIds[i] !== undefined){
+					bboyContent += '<iframe title="YouTube video player" class="youtube-player" type="text/html" src="https://www.youtube.com/embed/'+bboyIds[i]+'" frameborder="0" allowFullScreen></iframe>';
+				} else {
+					bboyContent += '<div class="novideo"><p>Coming Soon!</p></div>';
+				}
 			}
+			bboyBool = true;
 		}
-		$('.changing-ttt-board').html(tttContent);
-		tttContent = "";
+		
+		$('.changing-ttt-board').html(bboyContent);
 	});
 
 	$('#danceCovers').on("click", function(){
-		for (var i = 0; i<9; i++){
-			if (danceCoverIds[i] !== undefined){
-				tttContent += '<iframe title="YouTube video player" class="youtube-player" type="text/html" src="https://www.youtube.com/embed/'+danceCoverIds[i]+'" frameborder="0" allowFullScreen></iframe>';
-			} else {
-				tttContent += '<div class="novideo"><p>Coming Soon!</p></div>';
+		if (!coverBool){
+			for (var i = 0; i<9; i++){
+				if (danceCoverIds[i] !== undefined){
+					coverContent += '<iframe title="YouTube video player" class="youtube-player" type="text/html" src="https://www.youtube.com/embed/'+danceCoverIds[i]+'" frameborder="0" allowFullScreen></iframe>';
+				} else {
+					coverContent += '<div class="novideo"><p>Coming Soon!</p></div>';
+				}
 			}
+			
+			coverBool = true;
 		}
-		$('.changing-ttt-board').html(tttContent);
-		tttContent = "";
+		
+		$('.changing-ttt-board').html(coverContent);
 	});
 
 	$('#adts').on("click", function(){
-		if ($('#adts').data("youtube-links") == "1"){
+		if (!adtBool){
 			for (var i = 0; i<9; i++){
 				if (adtYoutubeIds[i] !== undefined){
-					tttContent += '<iframe title="YouTube video player" class="youtube-player" type="text/html" src="https://www.youtube.com/embed/'+adtYoutubeIds[i]+'" frameborder="0" allowFullScreen></iframe>';
+					adtContent += '<iframe title="YouTube video player" class="youtube-player" type="text/html" src="https://www.youtube.com/embed/'+adtYoutubeIds[i]+'" frameborder="0" allowFullScreen></iframe>';
 				} else {
-					tttContent += '<div class="novideo"><p>Coming Soon!</p></div>';
+					adtContent += '<div class="novideo"><p>Coming Soon!</p></div>';
 				}
 			}
-			$('.changing-ttt-board').html(tttContent);
-			tttContent = "";
+			adtBool = true;
 		}
+		$('.changing-ttt-board').html(adtContent);
 		
 	});
 });
